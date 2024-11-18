17b2a8c668d7d49a26788f52bde246e03fc60e91
DMG <daniel.marce@mataro.epiaedu.cat>
2024-11-18 22:58:25 +0100

    feat: solve exercises 00 and 01

--- loops-conditionals/conditionals/exercise_00.js
+++ loops-conditionals/conditionals/exercise_00.js
@@ -8,3 +8,26 @@
     7. At the end of the program, print out: "--- Animal check complete ---"
     8. Change the animal to different values to make sure it works in all cases
 */
+
+function animalChecker(animal) {
+    window.console.log('--- Animal Checker Program ---');
+
+    if (animal === 'cat') {
+        window.console.log('Meow!');
+    } else if (animal === 'dog') {
+        window.console.log('Woof!');
+    } else {
+        window.console.log('Must be an alien');
+    }
+
+    window.console.log('--- Animal check complete ---');
+}
+
+let animal = 'cat';
+animalChecker(animal);
+
+animal = 'dog';
+animalChecker(animal);
+
+animal = 'alien';
+animalChecker(animal);
